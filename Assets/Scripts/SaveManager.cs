24d153cd33d5221dda9742aea454ecb9139af87f
bollinca <51376777+bollinca@users.noreply.github.com>
2022-02-03 08:15:10 -0500

    Start drafting SaveData
    - Create SaveData class for creation of json data storage
    - complete saveData() (untested) which will save json data to a file
    - name loadData() (empty function)

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class SaveManager : MonoBehaviour
 {
@@ -18,6 +19,26 @@ public class SaveManager : MonoBehaviour
             DontDestroyOnLoad(this);
         }
         Destroy(this);
+    }
+
+    public void saveData()
+    {
+        SaveData data = new SaveData();
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void loadData()
+    {
+
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string name = playerName;
+        public int score = highScore;
     }
 
 }
