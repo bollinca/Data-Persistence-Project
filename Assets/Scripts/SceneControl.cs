0ac7addaf20db05193fbd4e41f6a6ff59f7411cc
bollinca <51376777+bollinca@users.noreply.github.com>
2022-02-03 08:00:30 -0500

    Implement high score persistance
    - Made high score carry through across scenes
    -  Made Name Input functional.
    - Made name save along with high score (between scenes)

--- Assets/Scripts/SceneControl.cs
+++ Assets/Scripts/SceneControl.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneControl : MonoBehaviour
 {
+    public GameObject nameInput;
+
     public void StartGame()
     {
+        SaveManager.playerName = nameInput.GetComponent<TMPro.TextMeshProUGUI>().text;
         SceneManager.LoadScene(1);
     }
 }
